a24280fe616e4891e33107d501830e8a1a8292fa
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-07-24 18:43:45 +0300

    Add more log function to notify

--- src/modules/notify/controller-notify.ts
+++ src/modules/notify/controller-notify.ts
@@ -6,7 +6,10 @@ import { fetchDestination } from '@/helpers/fetch.ts';
 import { InternalServerError } from '@/middlewares/error/base.ts';
 import type { Notification, RequestParams } from '@/types.ts';
 
-export async function post(request: Request<RequestParams, void, Notification>, response: Response) {
+export async function post(
+  request: Request<RequestParams, void, Notification>,
+  response: Response
+) {
   const { id, type, relativeHumidity, NO2, PM10, PM2P5, pressure, CO, SO2, temperature, O3, PM1 } =
     request.body;
 
@@ -38,19 +41,21 @@ export async function post(request: Request<RequestParams, void, Notification>, 
 
   const formatted = dataFormatter(notification, targetId);
 
-  const serverResponse = await fetchDestination()
-    .post('/AQI/SendData', formatted)
-    .catch((err) => {
-      throw new InternalServerError(err.message);
-    });
+  console.log('Formatted notification:', formatted);
+
+  const serverResponse = await fetchDestination().post('/AQI/SendData', formatted);
+
+  console.log('Notification forwarded to the destination');
 
   if (!serverResponse.ok) {
-    console.error('Destination response:', await serverResponse.json());
+    const json = await serverResponse.json();
+    console.error('Destination response:', json);
     throw new InternalServerError('Error while forwarding the notification');
   }
 
-  console.log('Notification forwarded to the destination');
-  console.log('Destination response:', await serverResponse.json());
+  const json = await serverResponse.json();
+
+  console.log('Destination response:', json);
 
   response.status(200).send({ message: 'Notification received' });
 }
