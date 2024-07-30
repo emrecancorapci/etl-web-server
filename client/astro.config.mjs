0de0fc2f54988070f2e1f07a9a3ad706ebf7ebda
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-07-30 23:18:46 +0300

    Add client files

---
+++ client/astro.config.mjs
@@ -0,0 +1,17 @@
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
+  output: 'static',
+  srcDir: 'src',
+  publicDir: 'public',
+  outDir: '../public',
+});
