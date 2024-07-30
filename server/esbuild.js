107181d08f347b08e399969b26e7a6f086bd6ebe
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-07-30 15:21:49 +0300

    Update codebase to move source folder and update packages

--- server/esbuild.js
+++ server/esbuild.js
@@ -1,10 +1,10 @@
 import { build } from 'esbuild';
 
 build({
-  entryPoints: ['src/server.ts'],
+  entryPoints: ['src/main.ts'],
   bundle: true,
   platform: 'node',
   target: 'node20',
-  outfile: 'dist/server.cjs',
+  outfile: '../dist/server.cjs',
   format: 'cjs',
 });
