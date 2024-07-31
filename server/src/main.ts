6a3bc8b4771b39f31ce0ee1ad3e04eb47368314f
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-07-31 14:34:28 +0300

    Update to fix scripts in server package.json and main.ts to check .env values.

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,38 @@ import http from 'node:http';
 
 import app from './app.ts';
 
-const NODE_PORT = Number(process.env.NODE_PORT) || 5000;
+const {
+  SOURCE_API_URI,
+  SOURCE_CLIENT_ID,
+  SOURCE_CLIENT_SECRET,
+  TARGET_API_URI,
+  TARGET_USERNAME,
+  TARGET_PASSWORD,
+  NODE_PORT
+} = process.env;
+
+if (
+  !(
+    SOURCE_API_URI &&
+    SOURCE_CLIENT_ID &&
+    SOURCE_CLIENT_SECRET &&
+    TARGET_API_URI &&
+    TARGET_USERNAME &&
+    TARGET_PASSWORD
+  )
+) {
+  throw Error('Some required environment variables are not set');
+}
+
+if (!NODE_PORT) {
+  console.warn('NODE_PORT is not set, using default port 5000');
+}
+
+const PORT = Number(NODE_PORT) || 5000;
 
 http
   .createServer(app)
-  .listen(NODE_PORT)
+  .listen(PORT)
   .on('listening', () => {
-    console.log(`Server is running on http://localhost:${NODE_PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
   });
