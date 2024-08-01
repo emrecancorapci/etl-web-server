2028b50875d99d7cc2401e69a0c201ca1cc182e0
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-08-01 17:51:35 +0300

    Add destination id for subscription get endpoints, Update delete function to also delete id pairs

--- server/src/modules/subscription/controller.ts
+++ server/src/modules/subscription/controller.ts
@@ -1,8 +1,9 @@
 import type { Request, Response } from 'express';
 
+import { getTargetId, removePairBySourceId } from '@/handlers/machine-handler.ts';
 import { sourceFetch } from '@/helpers/fetch.ts';
 import { BadRequestError, InternalServerError } from '@/middlewares/error/base.ts';
-import type { RequestParams, SourceErrorResponse } from '@/types.ts';
+import type { RequestParams, SourceErrorResponse, SubscriptionResponse } from '@/types.ts';
 
 import { createSubscription } from './utils/create-subscription.ts';
 import { generateSubscriptionRequest } from './utils/generate-subscription-request.ts';
@@ -20,15 +21,21 @@ interface ResponseBody {
 }
 
 export async function getAll(_: Request, response: Response) {
-  const serverResponse = await sourceFetch().get('/orion/v2/subscriptions');
+  const rawResponse = await sourceFetch().get('/orion/v2/subscriptions');
 
-  if (!serverResponse.ok) {
-    const error = (await serverResponse.json()) as SourceErrorResponse;
+  if (!rawResponse.ok) {
+    const error = (await rawResponse.json()) as SourceErrorResponse;
 
     throw new InternalServerError(error.description);
   }
 
-  const data = await serverResponse.json();
+  const serverResponse = (await rawResponse.json()) as SubscriptionResponse[];
+
+  const data = serverResponse.map((subscription: SubscriptionResponse) => {
+    const targetId = getTargetId(subscription.subject.entities[0].id);
+
+    return { ...subscription, targetId };
+  });
 
   response.status(200).send({ data });
 }
@@ -36,19 +43,23 @@ export async function getAll(_: Request, response: Response) {
 export async function get(request: Request, response: Response) {
   let { id } = request.params;
 
-  const serverResponse = await sourceFetch().get(`/orion/v2/subscriptions/${id}`);
+  const rawResponse = await sourceFetch().get(`/orion/v2/subscriptions/${id}`);
 
-  if (!serverResponse.ok) {
-    if (serverResponse.status === 404) {
+  if (!rawResponse.ok) {
+    if (rawResponse.status === 404) {
       throw new BadRequestError('Subscription not found.');
     } else {
-      const error = (await serverResponse.json()) as SourceErrorResponse;
+      const error = (await rawResponse.json()) as SourceErrorResponse;
 
       throw new InternalServerError(error.description);
     }
   }
 
-  const data = await serverResponse.json();
+  const serverResponse = (await rawResponse.json()) as SubscriptionResponse;
+
+  const targetId = getTargetId(serverResponse.subject.entities[0].id);
+
+  const data = { ...serverResponse, targetId };
 
   response.status(200).send({ data });
 }
@@ -83,6 +94,18 @@ export async function post(
 export async function del(request: Request, response: Response) {
   let { id } = request.params;
 
+  const rawResponse = await sourceFetch().get(`/orion/v2/subscriptions/${id}`);
+
+  if (!rawResponse.ok) {
+    if (rawResponse.status === 404) {
+      throw new BadRequestError('Subscription not found.');
+    } else {
+      const error = (await rawResponse.json()) as SourceErrorResponse;
+
+      throw new InternalServerError(error.description);
+    }
+  }
+
   const serverResponse = await sourceFetch().del(`/orion/v2/subscriptions/${id}`);
 
   if (!serverResponse.ok) {
@@ -90,6 +113,10 @@ export async function del(request: Request, response: Response) {
 
     throw new InternalServerError(error.description);
   }
+
+  const getServerResponse = (await rawResponse.json()) as SubscriptionResponse;
+
+  removePairBySourceId(getServerResponse.subject.entities[0].id);
 
   response.status(204).send();
 }
