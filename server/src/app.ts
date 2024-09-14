5e7d7dc0d17d3afdcf706162166d1ec344649e47
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-09-14 15:11:32 +0300

    Update gitignore and server scripts to fix pm2 logging

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,7 +15,6 @@ dotenv.config();
 
 const NODE_ENV = process.env.NODE_ENV || 'dev';
 
-
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
