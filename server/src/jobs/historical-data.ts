5be1fb3f82195bd654839b567f1f4b3471ca3487
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-09-24 09:54:44 +0300

    Refactor historical-data.ts to remove unnecessary console.error statement

--- server/src/jobs/historical-data.ts
+++ server/src/jobs/historical-data.ts
@@ -63,7 +63,6 @@ async function fetchMissingData(id: string) {
         '\nResponse: ' +
         response.statusText
     );
-    console.error('Response:', await response.json());
     return;
   }
 
